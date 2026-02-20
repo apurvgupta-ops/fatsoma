3b8b34cce1f30a7cde366767b234b056a44baada
apurvgupta-ops <apurvgupta.124@gmail.com>
2026-02-20 12:11:54 +0530

    ecosystme file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,12 +2,13 @@ module.exports = {
   apps: [
     {
       name: "next-app",
-      script: "/",
+      script: "node_modules/next/dist/bin/next",
       args: "start",
       cwd: "./", // current working directory
       env: {
         NODE_ENV: "production",
         PORT: 4486,
+        AUTH_TRUST_HOST: true,
       },
     },
   ],
